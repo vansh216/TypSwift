885e754eb5961006a46c64c14ccba681eefc462d
vansh216 <216386028+vansh216@users.noreply.github.com>
2026-05-17 23:43:48 +0530

    Create the react@Vite for the control the frontend

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl font-bold'>
+        Lets start!!
+        </h1>
+    </>
+  )
+}
+
+export default App
