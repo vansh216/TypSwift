096066fe51ca8e80122478190bdd49a8550a084d
vansh216 <216386028+vansh216@users.noreply.github.com>
2026-05-22 00:57:23 +0530

    connect the auth api to the backend

---
+++ src/features/auth/api/auth.api.jsx
@@ -0,0 +1,11 @@
+import api from '../../../shared/api/axios';
+
+export const loginApi = async (email, password) => {
+  const res = await api.post('/auth/login', { email, password });
+  return res.data;
+};
+
+export const registerApi = async (username, email, password) => {
+  const res = await api.post('/auth/register', { username, email, password });
+  return res.data;
+};
