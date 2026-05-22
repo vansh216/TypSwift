565d2a8144c684ad3c3cbddd3a53a2e4da0ee4fe
vansh216 <216386028+vansh216@users.noreply.github.com>
2026-05-22 17:07:33 +0530

    connect the profile to the api

---
+++ src/features/profile/api/Profile.api.jsx
@@ -0,0 +1,16 @@
+import api from '../../../shared/api/axios';
+
+
+export const fetchUserStats = async () => {
+  const res = await api.get('/user/stats');
+  return res.data;
+};
+
+
+export const fetchUserHistory = async ({ page = 1, limit = 10 } = {}) => {
+  const res = await api.get('/user/history', {
+    params: { page, limit }
+  });
+  console.log(res.data)
+  return res.data;
+};
