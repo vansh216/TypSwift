3eaa7a3b7c21c17ad0d35c2b2502df74c4b6177f
vansh216 <216386028+vansh216@users.noreply.github.com>
2026-05-24 13:16:45 +0530

    create the test api

---
+++ src/features/test/api/test.api.jsx
@@ -0,0 +1,33 @@
+import api from '../../../shared/api/axios';
+
+
+// fetch random paragraph based on filters
+
+export const fetchParagraph = async ({ difficulty = 'medium', duration = 3 } = {}) => {
+    const res = await api.get('/test/paragraph', {
+        params: { difficulty, duration },
+    });
+    return res.data;
+};
+
+
+// submit completed test result
+
+export const submitTest = async ({
+    wpm,
+    accuracy,
+    duration,
+    errors,
+    wpmHistory,
+    paragraphId,
+}) => {
+    const res = await api.post('/test/submit', {
+        wpm,
+        accuracy,
+        duration,
+        errors,
+        wpmHistory,
+        paragraphId,
+    });
+    return res.data;
+};
